7c629b7da2b90f858c3707c62a83f630b78fc3e1
mjung1798 <mor2222@naver.com>
2019-02-19 16:59:39 +0900

    get All version List

--- src/components/VersionPage/Listing.vue
+++ src/components/VersionPage/Listing.vue
@@ -1,16 +1,54 @@
 <template>
-  <li>
-    {{ todo.text }}
-  </li>
+  <div class="container">
+    <h2>Version List</h2>
+
+    <button v-on:click="getVersionList">getAllVersion</button>
+    <table>
+      <thead>
+        <tr>
+          <td>version</td>
+					<td>full</td>
+					<td>patch</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="elt in versionList">
+					<td>{{elt.version}}</td>
+					<td>{{elt.full}}</td>
+					<td>{{elt.patch}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
+import { fileServer } from "../../api/fileServer";
+
 export default {
-  props: {
-    todo: {
-      type: Object,
-      required: true
+  name: "TodoPage",
+  data() {
+    return {
+      versionList: []
+    };
+  },
+  methods: {
+    getVersionList() {
+      var vm = this;
+      fileServer
+        .getVersionList()
+        .then(response => {
+          console.log(response.data.responseData);
+          vm.versionList = response.data.responseData.slice();
+          console.log(this.versionList);
+        })
+        .catch(err => {
+          this.responseTxt = "서버 에러";
+        });
     }
+  },
+  mounted() {
+    this.getVersionList();
   }
-}
+};
 </script>
--- src/pages/VersionPage.vue
+++ src/pages/VersionPage.vue
@@ -1,92 +1,32 @@
 <template>
     <div class="version">
         <h2>New Version Input</h2>
-        <ul v-if="todos.length">
-			<Listing
-				v-for="todo in todos"
-				:key="todo.id"
-				:todo="todo"
-			/>
-		</ul>
-        <form @submit.prevent="onSubmit" name="fileForm" ref="fleForm">
-            <div>
-                <label for="newVersion"></label>
-                <input class="form-control" type="text" name="version"  v-model="version" autofocus placeholder="newVersion"/>
-                <label for="file"></label>
-                <input class="form-control" type="file" name ="sourceFile"  ref="file" v-on:change="handleFileUpload()"/>
-            </div>
-            <button class="btn" :class="{'btn-success' : !invalidForm}" type="submit" :disabled="invalidForm">Upload New Version</button>
-            <p>{{ responseTxt }}</p>
-        </form>
+        <versionUp/>
+        <Listing/>
+        
     </div>
 </template>
 
 <script>
-import {upload} from '../api/fileServer'
 import VueRouter from 'vue-router';
 import Listing from '../components/VersionPage/Listing.vue'
-
-let nextTodoId = 1
+import VersionUp from '../components/VersionPage/VersionUp'
 
 export default {
     components: {
-        Listing
-        
+        Listing,
+        VersionUp
     },
     data() {
         return {
-            version: '',
-            sourceFile: '',
             rPath : '',
-            responseTxt: '',
-            todos: [ //listing에 넘겨줘야할 값들
-				{
-					id: nextTodoId++,
-					text: 'Learn Vue'
-				},
-				{
-					id: nextTodoId++,
-					text: 'Learn about single-file components'
-				},
-				{
-					id: nextTodoId++,
-					text: 'Fall in love'
-				}
-			]
         }
     },
-    computed : {
-        invalidForm() {
-            return !this.version 
-        }
-    },created() {
+    created() {
         this.rPath = this.$route.query.rPath || '/home'
     },
-    methods : {
-        handleFileUpload(){
-            this.sourceFile = this.$refs.file.files;
 
-        },
-        onSubmit() {
-            
-            
-            console.log(this.version)
-            console.log(this.sourceFile[0])
-
-            let bodyFormData = new FormData();   
-            bodyFormData.set('version', this.version);   
-            bodyFormData.append('sourceFile', this.sourceFile[0]); 
-
-            upload.newVersion(bodyFormData)
-            .then(response => {
-                this.responseTxt = response.data.responseMessage
-                console.log(response.data.responseMessage);
-            })
-            .catch(err => {
-                this.responseTxt = "서버 에러";
-            })
-        }
-    }
+    
 }
 
 </script>
